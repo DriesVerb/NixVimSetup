ac43e5625ff3668ad7bd0d36e93dd31e206eb1c2
Dries Verboven <verboven.dries@gmail.com>
2024-07-13 13:44:57 +0200

    adding options and keymaps

--- config/core/keymaps/default.nix
+++ config/core/keymaps/default.nix
@@ -1,18 +1,113 @@
 {
   keymaps = [
 
-    #insert mode
+    ##insert mode
     {
+     mode  = "i";
      action = "<ESC>";
-     key =  "<leader>jk";
-     mode = "i";
+     key =  "jk";
     }
 
-	
-    #nvim-tree
+    ##normal mode
+    {
+   	mode = "n";
+      	action = ":q<CR>";
+	key = "<leader>qf";
+    }
+    {
+   	mode = "n";
+      	action = ":qa<CR>";
+	key = "<leader>qa";
+    }
+    {
+   	mode = "n";
+      	action = ":w<CR>";
+	key = "<leader>sf";
+    }
+    {
+   	mode = "n";
+      	action = ":wa<CR>";
+	key = "<leader>sa";
+    }
+
+    #clear highlights
+    {
+   	mode = "n";
+      	action = ":nohl<CR>";
+	key = "<leader>nh";
+    }
+
+    # toggle line numbers
+    {
+   	mode = "n";
+      	action = ":set rnu!<CR>";
+	key = "<leader>ln";
+    }
+
+    ## tabs
+    # open new tab
+    {
+   	mode = "n";
+      	action = ":tabnew<CR>";
+	key = "<leader>to";
+    }
+    # close current tab
+    {
+   	mode = "n";
+      	action = ":tabclose<CR>";
+	key = "<leader>tx";
+    }
+    # go to next tab
+    {
+   	mode = "n";
+      	action = ":tabn<CR>";
+	key = "<leader>tn";
+    }
+    # go to previous tab
+    {
+   	mode = "n";
+      	action = ":tabp<CR>";
+	key = "<leader>tp";
+    }
+
+    ##visual mode
+    #move code
+    {
+   	mode = "v";
+      	action = ":m '>+1<CR>gv=gv";
+	key = "J";
+    }
+    {
+   	mode = "v";
+      	action = ":m '>-2<CR>gv=gv";
+	key = "K";
+    }
+
+    ## nvim-tree
+    # toggle file explorer
     {
       action = "<cmd>NvimTreeToggle<CR>";
       key = "<leader>ee";
+    }
+    # toggle file in explorer
+    {
+      action = "<cmd>NvimTreeFindFile<CR>";
+      key = "<leader>ef";
+    }
+    # toggle file explorer on current file
+    {
+      action = "<cmd>NvimTreeFindFileToggle<CR>";
+      key = "<leader>et";
+    }
+    # collape file explorer 
+    {
+      action = "<cmd>NvimTreeCollapse<CR>";
+      key = "<leader>ec";
+    }
+    # refresh file explorer
+    {
+      action = "<cmd>NvimTreeRefresh<CR>";
+      key = "<leader>er";
     }
   ];
 }
--- config/core/options/default.nix
+++ config/core/options/default.nix
@@ -1,9 +1,50 @@
 {
   # vim.opt
   globalOpts = {
+    #line numbers
     relativenumber = true;
     number = true;
+    #space of the numbers
     numberwidth = 5;
+    ## tabs
+    # 2 spaces for tabs
+    tabstop = 2;
+    # 2 spaces for indent width
+    shiftwidth = 2;
+    # expand tab to spaces
+    expandtab = true;
+    #copy indent form current line
+    autoindent = true;
+
+    # line wrapping
+    wrap = true;
+
+    ## search
+    # ignore case
+    ingorecase = true;
+    # include mixed case in search
+    smartcase = true;
+    
+    # takes gui colors
+    termguicolors = true;
+    # prefers dark in schemes
+    background = "dark";
+
+    # allow backspace to indent
+    backspace = "indent,eol,start";
+
+    ## split windows
+    splitright = true;
+    splitbelow = true;
+
+    # clipboard
+    clipboard = "unnamed";
+
+    # swapping files and undos
+    swapfile = false;
+    backup = false;
+    undodir = os.getenv("home") .. "/.vim/ndodir";
+    undofile = true;
   };
 
   #vim.g
