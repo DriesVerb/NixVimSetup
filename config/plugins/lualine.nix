4b21ed27f4b05d4b25064f197a902aec6376a4e5
Dries Verboven <verboven.dries@gmail.com>
2024-07-15 21:26:36 +0200

    adding lua to lualine

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -1,7 +1,72 @@
 {
+  config = {
+      extraConfigLuaPre = ''
+        
+        		local symbolset = {
+			"",
+			"󰹼",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"󰱲",
+			"󰈷",
+			"󰉊",
+			"󱜜",
+			"󱨛",
+			"󰊠",
+			"󱍢",
+			"󰩐",
+			"󰩘",
+			"󰼁",
+			"󰧻",
+			"󰧿",
+			"󱠦",
+			"󱅻",
+			"󱇽",
+			"󰟟",
+			"󱙰",
+			"󰸌",
+			"󱅗",
+			"󱝅",
+			"󱙹",
+			"󰨈",
+			"󱕄",
+			"󱚥",
+			"󱄔",
+			"󰦥",
+			"󰟶",
+			"󰿧",
+			"󱗂",
+			"󰘉",
+			"󰑣",
+			"󱄾",
+			"󰹅",
+		}
+
+		math.randomseed(os.time())
+		local random_elem = symbolset[math.random(1, #symbolset)]
+
+-- stylua: ignore
+local symbols = {
+  unix = random_elem, -- e712
+  dos = '', -- e70f
+  mac = '', -- e711
+}
+
+		lualine.setup({
+			options = {
+				symbols = symbols,
+			},
+		})
+      '';
   plugins = {
     lualine = {
       enable = true;
     };
+  };
   };
 }
