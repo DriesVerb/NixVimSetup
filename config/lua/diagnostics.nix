5181ad8f720f7c47851fd494c4455a6f91683548
Dries Verboven <verboven.dries@gmail.com>
2024-07-17 19:33:53 +0200

    adding new diagnostics to nixvim

--- config/lua/diagnostics.nix
+++ config/lua/diagnostics.nix
@@ -1,20 +1,26 @@
 {
   config = {
     extraConfigLua = ''
-      vim.diagnostic.config({
-      		virtual_text = false, -- disable virtual text
-      		update_in_insert = true,
-      		underline = true,
-      		severity_sort = true,
-      		float = {
-      			focusable = true,
-      			style = "minimal",
-      			border = "rounded",
-      			source = "always",
-      			header = "",
-      			prefix = "",
-      		},
-      	}),
+          -- Change the Diagnostic symbols in the sign column (gutter)
+      		local signs = { Error = "󱚢 ", Warn = "󱚠 ", Hint = "󱙺 ", Info = "󱚦 " }
+      		for type, icon in pairs(signs) do
+      			local hl = "DiagnosticSign" .. type
+      			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      		end
+            vim.diagnostic.config({
+            		virtual_text = false, -- disable virtual text
+            		update_in_insert = true,
+            		underline = true,
+            		severity_sort = true,
+            		float = {
+            			focusable = true,
+            			style = "minimal",
+            			border = "rounded",
+            			source = "always",
+            			header = "",
+            			prefix = "",
+            		},
+            	})
     '';
   };
 }
