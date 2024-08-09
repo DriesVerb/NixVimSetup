2ef4c93cf29be397be30c5cdd114c344eef6f13d
Dries Verboven <verboven.dries@gmail.com>
2024-08-09 09:36:12 +0200

    adding placeholder hash

--- config/plugins/gotoVue.nix
+++ config/plugins/gotoVue.nix
@@ -6,7 +6,7 @@
         owner = "catgoose";
         repo = "vue-goto-definition.nvim";
         rev = "cefd5147d5892b75e3c017bcfb46d2a776275088";
-        hash = "";
+        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
       };
     })
   ];
